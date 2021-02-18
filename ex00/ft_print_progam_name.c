ec76b2777690625040f5e0f874d9237a130a9125
Maryna SAMOILENKO <msamoile@e3r9p6.42.fr>
2021-02-18 16:45:30 +0100

    00

---
+++ ex00/ft_print_progam_name.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_progam_name.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msamoile <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/18 16:03:48 by msamoile          #+#    #+#             */
+/*   Updated: 2021/02/18 16:44:00 by msamoile         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+int	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i])
+	{
+		i++;
+		write(1, &str[i])
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	(void) argc;
+	(void) argv;
+
+	ft_putstr(argv[0]);
+	write(1, "\n", 1);
+	return (0);
+}
